969d8a204977abd8db325b3f742dd2fd2bb26b1d
Damien Toma <damientoma.pro@gmail.com>
2015-07-25 18:32:20 +0800

    Add install script; fix git color

--- .bashrc
+++ .bashrc
@@ -1,2 +1,2 @@
 # Nothing to see here — Everything's in .bash_profile
-source ~/.bash_profile
+source $HOME/.bash_profile
---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo 'Install bashrc'
+ln -s `pwd`/.bashrc $HOME/.bashrc
+
+echo 'Install bash_profile'
+ln -s `pwd`/.bash_profile $HOME/.bash_profile
+
+echo 'Install vimrc'
+ln -s `pwd`/.vimrc $HOME/.vimrc
+
+echo 'Install gitconfig'
+ln -s `pwd`/.gitconfig $HOME/.gitconfig
+
+echo 'Install gitignore_global'
+ln -s `pwd`/.gitignore_global $HOME/.gitignore_global
